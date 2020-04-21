6763dda1a6120319884b3653940ab03ba43846fb
cScheiwi <christoph.scheiwiller@hsr.ch>
2020-04-21 17:03:54 +0200

    cleanup

--- src/main/java/ch/hsr/greatnamebackend/GreatnameBackendApplication.java
+++ src/main/java/ch/hsr/greatnamebackend/GreatnameBackendApplication.java
@@ -1,6 +1,5 @@
 package ch.hsr.greatnamebackend;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +10,6 @@ public class GreatnameBackendApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreatnameBackendApplication.class, args);
-	}
-
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
 	}
 
 }
